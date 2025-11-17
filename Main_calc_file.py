47ea2f65326906dbf082305259d1ed5fc90fdbd4
Alef2020 <66664532+Alef2020@users.noreply.github.com>
2025-11-17 15:15:01 +0100

    Update Main_calc_file.py

--- Main_calc_file.py
+++ Main_calc_file.py
@@ -1,6 +1,33 @@
-#When you add new variables, first check they don't already exist with a different name
+#When you add new variables:
+#1. Check they don't already exist with a different name
+#2. Give it a resonable name
+#3. Add units in comment in square brackets
+#4. Explain what the variable describes
+
 
 
 #-----------------------------
-#Set up of variables
+#Set up of variables - beginning
 #-----------------------------
+
+#the values below are arbitrary for now
+t = 10 #[mm] thickness of the flange, reffer to fig. 4.1
+D1 = 10 #[mm] diameter of the hole in flange, reffer to fig. 4.1
+E = 7.31e9#[Pa] Young's modulus
+
+#-----------------------------
+#Set up of variables - ending
+#-----------------------------
+
+
+#----------------------------
+#Example section - beginning
+#---------------------------
+
+#calculates the product of thickness of flange and Young's modulus
+print(t/E,"In arbitrary units")
+
+
+#----------------------------
+#Example section - ending
+#---------------------------
