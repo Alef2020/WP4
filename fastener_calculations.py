b14dbcaa97885dd6a6457e77e139aa2118c70eaf
jschoevaars-a11y <jschoevaars@gmail.com>
2025-11-24 14:07:49 +0100

    Refactor fastener calculations and class structures
    Refactor fastener calculations to improve clarity and structure. Adjusted load case and material properties classes, and updated fastener configuration handling.

--- fastener_calculations.py
+++ fastener_calculations.py
@@ -1,24 +1,24 @@
 import math
 
-class MaterialProperties(): #class for material properties
-    def __init__(self, E = [0,0,0],  rho = 0, strength = 0, shear = 0): #more properties could be added is needed
-        self.Youngs_modulus = E
-        self.shear_strengh = shear
-        self.density = rho
-        self.stress_yield = strength
-
-#example materials
-Alumunium = MaterialProperties(E = [70e9,70e9,70e9], rho = 2700) #example material
-Steel = MaterialProperties(E = [200e9,200e9,200e9], rho = 7850) #example material
-
-class Load_Case(): #Define one load case
+class Load_Case(): #Define one load, this is more for clarity, it could also just be an array
     def __init__(self, Fx = 0, Fy = 0, Fz = 0, Mx = 0, My = 0, Mz = 0):
         self.Force_X = Fx
         self.Force_Y = Fy
         self.Force_Z = Fz
         self.Moment_X = Mx
         self.Moment_Y = My
-        self.Moment_Z = Mz      
+        self.Moment_Z = Mz    
+
+class MaterialProperties(): #class for material properties
+    def __init__(self, E,  rho = 0, strength = 0, shear = 0): #more properties could be added is needed
+        self.Youngs_modulus = E
+        self.density = rho
+        self.stress_yield = strength
+        self.shear_strenght = shear if shear != 0 else 1.5*self.stress_yield**2 # weird relation as given in WP4, I think its not right yet
+
+#example materials
+Alumunium = MaterialProperties(E = 70e9, rho = 2700) #example material
+Steel = MaterialProperties(E = 200e9, rho = 7850) #example material  
 
 class Fastener_Configuration(): #Define one design configuration
     def __init__(self, d_h = 0.0075, d_f = 0.007, d_o = 0.004, material = Steel): #standard M4-steel bolt
@@ -38,8 +38,8 @@ class Fastener(): #Define one fastener with its properties and load cases
         self.load_cases = load_cases
 
 class design_Configuration(): #Define one design configuration
-    def __init__(self, fastener_positions, load_cases = [], t_1 = 3, t_2 = 4, t_3 = 6, fastener_config = Fastener_Configuration): #any default values are bs, will be fixed with the first iteration
-        #other parameters
+    def __init__(self, fastener_positions, load_cases = [], t_1 = 3, t_2 = 4, t_3 = 6, fastener_config = Fastener_Configuration):
+        #other parameters these can be changed for other purposes
         self.t_1 = t_1
         self.t_2 = t_2
         self.t_3 = t_3
@@ -52,17 +52,30 @@ class design_Configuration(): #Define one design configuration
         self.fastener_config = fastener_config
         self.A_i = math.pi*(self.fastener_config.d_o/2)**2
 
-        self.FastenerList = [] #list of fasteners objects containing material properties, dimensions
+        #This section of the code calculates the c.g. of the fastener pattern and ajust the positions to make that the origin
+        CG = [0,0.0]
+        CG[0] = sum([pos[0] for pos in self.fastener_positions])/self.n_f #can prob be done better with np
+        CG[2] = sum([pos[2] for pos in self.fastener_positions])/self.n_f
+        for Fastner_pos in self.fastener_positions:
+            Fastner_pos[0] -= CG[0]
+            Fastner_pos[2] -= CG[2]
         
+        for LC in self.load_cases:
+            LC.Moment_X = -CG[2]*LC.Force_Y 
+            LC.Moment_Z = CG[0]*LC.Force_Y #adjust moments based on new origin
+
+        #This section calculates the area moment of inertia (sort of; the equivelant calculation to get the distribution of forces on each fastener) and creates fastener objects for each position
+        self.FastenerList = [] #list of fasteners objects containing material properties, dimensions
         for Fastener_pos in self.fastener_positions:
             self.area_moment[0] += self.A_i*Fastener_pos[0]**2 #approximation of 2nd moment of area (x-axis)
             self.area_moment[1] += self.A_i*(Fastener_pos[0]**2 + Fastener_pos[2]**2) #approximation of polar moment of inertia (y-axis))
             self.area_moment[2] += self.A_i*Fastener_pos[2]**2 #approximation of 2nd moment of area around z-axis  
             self.FastenerList.append(Fastener(Fastener_pos ,self.fastener_config))
-        
-        for f in self.FastenerList: #This part approximates an area moment of inertia
+
+        #This section goes through each loadcase for the entire structure, calculates the forces on each fastener and stores them in a fastener object
+        for f in self.FastenerList: 
             local_load_cases = []
-            for LC in self.load_cases: #This loop goes through each loadcase for the entire structure, calculates the forces on each fastener and stores them in a fastener object
+            for LC in self.load_cases: 
                 F_p = Load_Case(0,0,0,0,0,0)
                 F_p.Force_X = LC.Force_X / self.n_f + LC.Moment_Y * f.position[2] * self.A_i / self.area_moment[2]
                 F_p.Force_Y = LC.Force_Y / self.n_f + LC.Moment_Z * f.position[0] * self.A_i / self.area_moment[0] - LC.Moment_X * f.position[2] * self.A_i / self.area_moment[2]
@@ -160,6 +173,7 @@ for LC in Total_load_cases:
         print("Force distribution correct.")
     
     print(f"Fx: {Total_Fx}, Fy: {Total_Fy}, Fz: {Total_Fz}, Mx: {Total_Mx}, My: {Total_My}, Mz: {Total_Mz}")
+
 
 
 
